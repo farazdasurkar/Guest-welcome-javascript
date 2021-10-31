5547a83ee9eb5681de85f31720a6e73961007bb7
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-31 23:00:48 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,8 @@
+var guiestList = ["Ram", "Afnan ", "faraz", "Ali", "Saif"]
+var gusest = prompt("what is Your Name");
+if (guiestList.includes(gusest)) {
+    alert("WELCOME");
+}
+else {
+    alert("Not Welcome");
+}
